a53276ef4907416d2a51c6669ef45cc282af7955
mizugorou2019 <mizugorou2019@users.noreply.github.com>
2020-08-21 15:58:09 +0900

    AjaxAppの準備

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,15 +1,16 @@
 class PostsController < ApplicationController
   def index
-     @posts = Post.all
+     @posts = Post.all.order(id: "DESC")
   end
 
-  def new
-  end
+  # def new
+  # end
 
   def create
-    Post.create(memo: params[:memo])
+    Post.create(content: params[:content])
     # モデル名.create(保存したいカラム名:(つまりキー) 保存する値(つまりバリュー))
-    # 「memo」という名前の箱でへデータを送り保存する
+    # 「content」という名前の箱でへデータを送り保存する
+    redirect_to action: :index
   end
 
 end
